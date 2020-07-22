f92b193c63a330ffe4beefcb5118127517888e2c
Cornelii <onelife0985@gmail.com>
2020-07-22 22:03:59 +0900

    All_Complete_Car

--- v1/my_car.py
+++ v1/my_car.py
@@ -1,9 +1,12 @@
 from DrivingInterface.drive_controller import DrivingController
 import numpy as np
 
+
 class ToGoal:
-    def __init__(self):
+    def __init__(self, is_debug=False):
         self.DUMMY = 99
+        self.utils = Utils()
+        self.is_debug = is_debug
 
     def preprocessing(self, sensing_info):
         data = sensing_info
@@ -12,62 +15,29 @@ class ToGoal:
     def modify_input(self, data, half_road_limit=7):
         origin_to_middle = data.to_middle
         origin_moving_angle = data.moving_angle
-        M = 5
-        N = 2 * M
-        obstacle_list = [self.DUMMY]*N
-        data.obstacle_flag = False
 
-        d = data.speed / 3.6 * 1.5 + 5
-        delta = 2 * (half_road_limit) / N
-        print(delta)
-        print(half_road_limit)
+        # get obstacle_map
+        # m = 5
+        self.utils.get_obstacle_map(data, half_road_limit)
 
-        for obstacle in data.track_forward_obstacles:
-            # make N partition on Road and get obstacle_list
-            if 2 <= obstacle["dist"] <= d:
-                for i in range(N):
-                    if obstacle_list[i] == self.DUMMY and obstacle["to_middle"] < delta * i - half_road_limit:
-                        obstacle_list[i] = obstacle["to_middle"]
-                        break
-        # get target to_middle_target
-        i = 0
-        a, b = 0, 0
-        point = 0
-        while i < N:
-            if obstacle_list[i] == self.DUMMY:
-                point = i
-                for j in range(i + 1, N):
-                    if obstacle_list[j] == self.DUMMY:
-                        if j == N-1:
-                            b = N-1
-                            a = point
-                            i = j
-                        continue
-                    else:
-                        if (j - 1 - i) > b - a:
-                            a = i
-                            b = j - 1
-                        i = j
-                        break
+        # get to_middle to pass for obstacles
+        to_middle_target, a, b = self.utils.get_optimal_pass_mid_point(data)
 
-            i += 1
-        print(obstacle_list)
-        print(f"a:{a} b:{b}")
-        to_middle_target = delta * (a + b + 1 - N) / 2
-        min_d_obstacle = delta
-        if a > 0:
-            min_d_obstacle = abs(obstacle_list[a-1] - to_middle_target)
-        if b < N - 1 and obstacle_list[b+1] != self.DUMMY:
-            if min_d_obstacle > abs(obstacle_list[b-1] - to_middle_target):
-                min_d_obstacle = abs(obstacle_list[b-1] - to_middle_target)
+        actual_min_distance = self.utils.get_min_distance_to_obstacle(data, to_middle_target, a, b)
 
-        data.to_middle = np.dot(np.array([-0.8, 1.0, -0.001]), np.array([to_middle_target, origin_to_middle,
-                                                       origin_moving_angle]))
-        data.moving_angle = np.dot(np.array([-0.8, -0.01, 1.0]), np.array([to_middle_target, origin_to_middle,
-                                                          origin_moving_angle]))
+        if data.obstacle_flag:
+            data.to_middle = np.dot(np.array([-0.8, 1.05, -0.001]), np.array([to_middle_target, origin_to_middle,
+                                                                             origin_moving_angle]))
+            data.moving_angle = np.dot(np.array([-0.8, -0.01, 1.0]), np.array([to_middle_target, origin_to_middle,
+                                                                             origin_moving_angle]))
 
-        if sum(obstacle_list) != 990:
-            data.obstacle_flag = True
+        if self.is_debug:
+            print(data.obstacle_map)
+            print(f"a: {a}, b: {b}")
+            print(f"to_middle_target: {to_middle_target}")
+            print(f"obstacle_flag: {data.obstacle_flag}")
+            print(f"min_distance: {actual_min_distance}")
+
         return data
 
     def get_final_input(self, data):
@@ -79,25 +49,20 @@ class ToGoal:
         car_controls.steering += 0.013*data.track_forward_angles[0]
 
     def set_throttle(self, car_controls, data):
-        car_controls.throttle = 1
-        if data.moving_angle > 10:
-            car_controls.throttle = 0.7
-        if (data.speed >= 150):
-            car_controls.throttle = 0.8
-        if (data.speed >= 200):
-            car_controls.throttle = 0.3
-        if abs(data.moving_angle) > 10:
-            car_controls.throttle = 0.3
+        MIN_VELOCITY = 60
+        SLOPE = - 1 / 500
 
-        if data.lap_progress < 0.5 or data.speed < 50:
+        if data.speed <= MIN_VELOCITY:
             car_controls.throttle = 1
-
-        if data.obstacle_flag:
-            car_controls.throttle -= 0.1
-
+        else:
+            car_controls.throttle = 1 + SLOPE * (data.speed - MIN_VELOCITY)
+            if abs(data.moving_angle) > 10:
+                car_controls.throttle = 0.6
+            if data.track_forward_angles[9] < 10:
+                car_controls.throttle += 1 / (20 + abs(data.track_forward_angles[9])*0.1)
 
     def set_brake(self, car_controls, data):
-        if abs(data.track_forward_angles[9]) > 50:
+        if abs(data.track_forward_angles[9]) > 70:
             if data.speed > 80:
                 car_controls.brake = 0.2
                 car_controls.throttle = 0.7
@@ -109,8 +74,108 @@ class ToGoal:
             car_controls.brake = 0.3
             car_controls.throttle = 0.5
 
-    def get_sign(self, x):
-        return 1 if x >= 0 else -1
+    def cornering_strategy(self, data):
+        pass
+
+    def opponents_strategy(self, data):
+        pass
+
+    def escape_strategy(self, data):
+        pass
+
+class Utils:
+
+    def __init__(self):
+        self.DUMMY = 99
+
+    def get_obstacle_map(self, data, half_road_limit=7, m=None):
+        # function to get obstacle map in the "data"
+        # data => sensing_info - based object
+        # half_road_limit => car_object.half_road_limit
+        # m map-dividing factor map consists of 2*m list
+        # data.obstacle List[]  No obstacle -> self.DUMMY. if there exists its to_middle value is included
+        # data.delta = half_road_limit * 2 / (2 * m)
+
+        if m:
+            pass
+        else:
+            m = int(round(2*half_road_limit - 4)/2)
+        n = 2 * m
+        data.obstacle_map = [self.DUMMY] * n
+        data.obstacle_flag = False
+
+        forward_detection_range = data.speed / 3.6 * 1.7 + 5
+        forward_undetected_range = data.speed / 3.6 * 0.1
+        delta = 2 * half_road_limit / n
+
+        data.delta = delta
+        for obstacle in data.track_forward_obstacles:
+            # make N partition on Road and get obstacle_map
+            if forward_undetected_range <= obstacle["dist"] <= forward_detection_range:
+                for i in range(n):
+                    if data.obstacle_map[i] == self.DUMMY and obstacle["to_middle"] < delta * (i + 1) - half_road_limit:
+                        data.obstacle_map[i] = obstacle["to_middle"]
+                        if ~data.obstacle_flag:
+                            data.obstacle_flag = True
+                        break
+
+    def get_optimal_pass_mid_point(self, data, n=None):
+        # method to get kind of mid point of bypass to obstacle
+        # data: sensing_info based object
+        # n => length of obstacle map
+        # return pass_mid_point, a, b (a: leftmost index of the widest range to pass, b: rightmost index)
+
+        if data.obstacle_map:
+            pass
+        else:
+            raise Exception("Get Obstacle map First. Use get_obstacle_map(data, half_road_limit)")
+        if not n or len(data.obstacle_map) != n:
+            n = len(data.obstacle_map)
+        # get target to_middle_target
+        i = 0
+        a, b = 0, 0
+        # point = 0
+        while i < n:
+            if data.obstacle_map[i] == self.DUMMY:
+                # point = i
+                for j in range(i + 1, n):
+                    if data.obstacle_map[j] == self.DUMMY:
+                        if j == n - 1 and j - i > b - a:
+                            b = n - 1
+                            a = i
+                            i = j
+                        continue
+                    else:
+                        if (j - i) > b - a:
+                            a = i
+                            b = j - 1
+                        i = j
+                        break
+            i += 1
+        to_middle_target = data.delta * (a + b + 1 - n) / 2
+        return to_middle_target, a, b
+
+    def get_curvature(self):
+        pass
+
+    def get_min_distance_to_obstacle(self, data, to_middle_target, a, b, n = None):
+        # function for actual minimum distacne between obstacle and optimal_pass_middle_point
+
+        if data.obstacle_map:
+            pass
+        else:
+            raise Exception("Get Obstacle map First. Use get_obstacle_map(data, half_road_limit)")
+        if not n or len(data.obstacle_map) != n:
+            n = len(data.obstacle_map)
+
+        min_d_obstacle = data.delta
+        if a > 0:
+            min_d_obstacle = to_middle_target - data.obstacle_map[a-1]
+        if b < n - 1 and data.obstacle_map[b+1] != self.DUMMY:
+            if abs(min_d_obstacle) > abs(data.obstacle_map[b-1] - to_middle_target):
+                min_d_obstacle = to_middle_target - data.obstacle_map[b-1]
+        return min_d_obstacle
+
 
 class DrivingClient(DrivingController):
     def __init__(self):
@@ -119,9 +184,8 @@ class DrivingClient(DrivingController):
         # =========================================================== #
         # Editing area starts from here
         #
-
-        self.to_goal = ToGoal()
         self.is_debug = True
+        self.to_goal = ToGoal(self.is_debug)
 
         #
         # Editing area ends
